ab0f2c694e4ea35eb39594d2393875ac55ff4e04
elbiazo <elbiazo@icloud.com>
2024-05-20 13:33:25 -0400

    tab as four spaces

--- init.lua
+++ init.lua
@@ -155,6 +155,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- ELB: Set tabstop as 4 spaces
+vim.opt.tabstop = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 

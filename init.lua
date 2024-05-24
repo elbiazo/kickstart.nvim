226fe41efb612db78d7feaca44734e6df493ae75
elbiazo <elbiazo@icloud.com>
2024-05-24 17:35:44 -0400

    adding tabby

--- init.lua
+++ init.lua
@@ -263,6 +263,45 @@ require('lazy').setup({
   },
   -- ELB: end of Comment.nvim
 
+  -- ELB: adding tabby so that tab shows line number
+  {
+    'nanozuki/tabby.nvim',
+    event = 'VimEnter',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      -- configs...
+      local theme = {
+        fill = 'TabLineFill',
+        -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+        head = 'TabLine',
+        current_tab = 'TabLineSel',
+        tab = 'TabLine',
+        win = 'TabLine',
+        tail = 'TabLine',
+      }
+      require('tabby.tabline').set(function(line)
+        return {
+          line.tabs().foreach(function(tab)
+            local hl = tab.is_current() and theme.current_tab or theme.tab
+            return {
+              line.sep('', hl, theme.fill),
+              tab.is_current() and '' or '',
+              tab.number(),
+              tab.name(),
+              tab.close_btn '',
+              line.sep('', hl, theme.fill),
+              hl = hl,
+              margin = ' ',
+            }
+          end),
+          line.spacer(),
+          hl = theme.fill,
+        }
+      end)
+    end,
+  },
+  -- ELB: end of tabby
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
